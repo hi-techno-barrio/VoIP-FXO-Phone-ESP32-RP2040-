78360645a7373d39256b25b96ec7dfcd9af81311
Christopher Coballes <cobecoballes@gmail.com>
2023-03-06 16:47:10 +0800

    Create EXP1_ESP32.c

---
+++ EXP1_ESP32.c
@@ -0,0 +1,158 @@
+#include <Arduino.h>
+#include <math.h>
+
+#define ADC_PIN 36
+#define SAMPLE_RATE 8000
+#define SAMPLE_SIZE 8
+#define SAMPLE_BUFFER_SIZE 64
+
+const int16_t G711_QUANTIZATION_FACTOR = 32767;
+
+const uint8_t G711_EXPONENT_TABLE[128] = {
+  0, 0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3,
+  4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+  5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+  5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+  6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+  6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+  6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+  6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6
+};
+
+const uint8_t G711_COMPAND_TABLE[128] = {
+  0, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
+  16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
+  16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
+  16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
+  16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
+  
+16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
+16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
+16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
+16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16
+};
+
+const uint8_t ULAW_COMPAND_TABLE[256] = {
+0, 0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3,
+4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+7, 7, 7, 7, 7,7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,8, 8, 8, 8, 8, 
+8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8 };
+const uint8_t ALAW_COMPAND_TABLE[256] = {
+1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8,
+9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+25, 27, 29, 31, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44,
+45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+61, 62, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77,
+78, 79, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94,
+95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110,
+111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126,
+127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142,
+143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158,
+159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174,
+175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190,
+191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206,
+207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222,
+223, 224, 225, 226, 227, 228,228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243,
+244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, 255, 255, 255, 255
+};
+
+void setup() {
+Serial.begin(9600);
+}
+
+void loop() {
+// Read analog input
+int16_t analogValue = analogRead(A0);
+
+// Compress to uLaw and aLaw
+uint8_t uLawValue = compressULaw(analogValue);
+uint8_t aLawValue = compressALaw(analogValue);
+
+// Print the original and compressed values
+Serial.print("Analog Value: ");
+Serial.print(analogValue);
+Serial.print(" | uLaw Value: ");
+Serial.print(uLawValue);
+Serial.print(" | aLaw Value: ");
+Serial.println(aLawValue);
+
+delay(100);
+}
+
+uint8_t compressULaw(int16_t sample) {
+const uint8_t BIAS = 0x84;
+uint8_t sign, exponent, mantissa, compressedByte;
+
+// Convert the sample to sign-magnitude
+if (sample < 0) {
+sign = 0x00;
+sample = -sample;
+} else {
+sign = 0x80;
+}
+
+// Apply the uLaw companding
+if (sample > MAX_ANALOG_VALUE) {
+sample = MAX_ANALOG_VALUE;
+}
+
+sample = (uint32_t)sample * (1 << (BIT_DEPTH - 1)) / MAX_ANALOG_VALUE;
+exponent = (uint8_t)log2(sample);
+mantissa = sample >> (exponent + (BIT_DEPTH - 8));
+compressedByte = ~((sign | ((exponent + EXP_BIAS) << MANTISSA_BITS) | mantissa) ^ 0xFF);
+compressedByte |= BIAS;
+
+return compressedByte;
+}
+
+uint8_t compressALaw(int16_t sample) {
+const uint8_t BIAS = 0x55;
+uint8_t sign, exponent, mantissa, compressedByte;
+
+// Convert the sample to sign-magnitude
+if (sample < 0) {
+sign = 0x80;
+sample = -sample;
+} else {
+sign = 0x00;
+}
+
+// Apply the aLaw companding
+if (sample > MAX_ANALOG_VALUE) {
+sample = MAX_ANALOG_VALUE;
+}
+
+sample = (uint32_t)sample * (1 << (BIT_DEPTH - 1)) / MAX_ANALOG_VALUE;
+exponent = (uint8_t)log2(sample);
+mantissa = sample >> (exponent + (BIT_DEPTH - 8));
+compressedByte = ~(sign | ((exponent << MANTISSA_BITS) | mantissa));
+compressedByte ^= 0x55;
+compressedByte |= BIAS;
+
+return compressedByte;
+}
+
