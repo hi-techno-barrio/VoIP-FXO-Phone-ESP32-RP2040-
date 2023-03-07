c36037a3cf7c4896c0f6e655db5bb552cce3b8d6
Christopher Coballes <cobecoballes@gmail.com>
2023-03-07 23:18:46 +0800

    Update lib_audio.c

--- DESIGN-IAX2/lib_audio.c
+++ DESIGN-IAX2/lib_audio.c
@@ -1,8 +1,19 @@
 #include "audio.h"
 
 void AudioProcessing::filter(uint8_t *input, uint8_t *output, int inputSize, int outputSize) {
-  // TODO: Implement audio filtering algorithm
+  // Implement a simple low-pass filter
+  float alpha = 0.2;
+  int16_t lastSample = 0;
+  
+  for (int i = 0; i < inputSize; i += 2) {
+    int16_t sample = ((int16_t)input[i]) | (((int16_t)input[i+1]) << 8);
+    sample = (alpha * sample) + ((1 - alpha) * lastSample);
+    lastSample = sample;
+    output[i] = sample & 0xFF;
+    output[i+1] = (sample >> 8) & 0xFF;
+  }
 }
+
 
 void AudioProcessing::amplify(uint8_t *input, uint8_t *output, int inputSize, int outputSize, float gain) {
   for (int i = 0; i < inputSize; i++) {
