b65a1fbc04afc7640761abe1bf2cac4871ec4a83
Christopher Coballes <cobecoballes@gmail.com>
2023-03-07 23:43:26 +0800

    Update lib_audio.c

--- DESIGN-IAX2/lib_audio.c
+++ DESIGN-IAX2/lib_audio.c
@@ -91,44 +91,162 @@ class uLawCodec : public Codec {
 
 class aLawCodec : public Codec {
   public:
-    const char* getName() { return"aLaw"; }
+    const char* getName() { return "aLaw"; }
+    int getSampleRate() { return 8000; }
+    int getBitsPerSample() { return 8; }
+    int getBlockSize() { return 160; }
+
+    void compress(uint8_t *input, uint8_t *output, int inputSize) {
+      for (int i = 0; i < inputSize; i++) {
+        int16_t sample = ((int16_t)input[i*2]) | (((int16_t)input[i*2+1]) << 8);
+        sample = sample >> 3;
+        int sign = (sample >> 8) & 0x80;
+        if (sign) {
+          sample = -sample;
+        }
+        if (sample > 32635) {
+          sample = 32635;
+        }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        
+class G711Codec : public Codec {
+public:
+const char* getName() { return "G.711"; }
 int getSampleRate() { return 8000; }
 int getBitsPerSample() { return 8; }
 int getBlockSize() { return 160; }
- void compress(uint8_t *input, uint8_t *output, int inputSize) {
+void compress(uint8_t *input, uint8_t *output, int inputSize) {
   for (int i = 0; i < inputSize; i++) {
     int16_t sample = ((int16_t)input[i*2]) | (((int16_t)input[i*2+1]) << 8);
-    sample = sample >> 3;
-    int sign = (sample >> 8) & 0x80;
-    if (sign) {
-      sample = -sample;
-    }
-    if (sample > 32635) {
-      sample = 32635;
-    }
-    if (sample >= 256) {
-      int exponent = 7 - (int)log2(sample);
-      int mantissa = (sample >> (exponent + 3)) & 0x0F;
-      output[i] = sign | (exponent << 4) | mantissa;
-    } else {
-      output[i] = sign | (sample >> 4);
-    }
-    output[i] ^= 0x55;
+    output[i] = linear2alaw(sample);
   }
 }
 
 void decompress(uint8_t *input, uint8_t *output, int outputSize) {
   for (int i = 0; i < outputSize; i++) {
-    int sign = input[i] & 0x80;
-    int exponent = (input[i] >> 4) & 0x07;
-    int mantissa = input[i] & 0x0F;
-    int sample = (mantissa << (exponent + 3));
-    sample |= 1 << (exponent + 2);
-    if (sign) {
-      sample = -sample;
-    }
+    int16_t sample = alaw2linear(input[i]);
     output[i*2] = sample & 0xFF;
     output[i*2+1] = (sample >> 8) & 0xFF;
   }
 }
- };
+private:
+uint8_t linear2alaw(int16_t pcm_val) {
+const int16_t ALAW_MAX = 0xFFF;
+const int16_t ALAW_BIAS = 0x84;
+int16_t mask;
+uint8_t aval;  if (pcm_val >= 0) {
+    mask = 0xD5;
+  } else {
+    mask = 0x55;
+    pcm_val = -pcm_val - 8;
+  }
+
+  if (pcm_val > ALAW_MAX) {
+    pcm_val = ALAW_MAX;
+  }
+
+  pcm_val += ALAW_BIAS;
+
+  if (pcm_val > 0x7F) {
+    aval = (uint8_t)(~mask | (pcm_val >> 1));
+  } else {
+    aval = (uint8_t)(mask | (pcm_val >> 1));
+  }
+
+  return aval;
+}
+
+int16_t alaw2linear(uint8_t a_val) {
+  const int16_t ALAW_BIAS = 0x84;
+  int16_t t, seg;
+  a_val ^= 0x55;
+  t = (a_val & 0x7F) << 4;
+  seg = ((unsigned)a_val & 0x70) >> 4;
+  switch (seg) {
+    case 0:
+      t += 8;
+      break;
+    case 1:
+      t += 0x108;
+      break;
+    default:
+      t += 0x108;
+      t <<= seg - 1;
+  }
+  return ((a_val & 0x80) ? t : -t) - ALAW_BIAS;
+}
+};
+
+class uLawG711Codec : public Codec {
+public:
+const char* getName() { return "uLaw-G.711"; }
+int getSampleRate() { return 8000; }
+int getBitsPerSample() { return 8; }
+int getBlockSize() { return 160; }void compress(uint8_t *input, uint8_t *output, int inputSize) {
+  uint8_t ulawInput[inputSize];
+  uLawCodec ulaw;
+  ulaw.compress(input, ulawInput, inputSize);
+
+  G711Codec g711;
+  g711.compress(ulawInput, output, inputSize);
+}
+
+void decompress(uint8_t *input, uint8_t *output, int outputSize) {
+  uint8_t g711Input[outputSize];
+  G711Codec g711
+class G711Codec : public Codec {
+public:
+const char* getName() { return "G.711"; }
+int getSampleRate() { return 8000; }
+int getBitsPerSample() { return 8; }
+int getBlockSize() { return 160; }void compress(uint8_t *input, uint8_t *output, int inputSize) {
+  for (int i = 0; i < inputSize; i++) {
+    int16_t sample = ((int16_t)input[i*2]) | (((int16_t)input[i*2+1]) << 8);
+    int8_t encodedSample = linearToMuLaw(sample);
+    output[i] = (uint8_t)encodedSample;
+  }
+}
+
+void decompress(uint8_t *input, uint8_t *output, int outputSize) {
+  for (int i = 0; i < outputSize; i++) {
+    int8_t encodedSample = (int8_t)input[i];
+    int16_t sample = muLawToLinear(encodedSample);
+    output[i*2] = sample & 0xFF;
+    output[i*2+1] = (sample >> 8) & 0xFF;
+  }
+}
+private:
+static int8_t linearToMuLaw(int16_t sample) {
+const int BIAS = 132;
+const int CLIP = 32635;
+const int MULAW_MAX = 0x1F;
+int sign = (sample >> 8) & 0x80;
+if (sign) {
+sample = ~sample;
+}
+if (sample > CLIP) {
+sample = CLIP;
+}
+sample += BIAS;
+int exponent = log2((sample >> 7) & 0xFF);
+int mantissa = (sample >> (exponent + 3)) & 0x0F;
+int encodedSample = ~(sign | (exponent << 4) | mantissa);
+return (int8_t)(encodedSample & MULAW_MAX);
+}
+
+static int16_t muLawToLinear(int8_t encodedSample) {
+  const int BIAS = 132;
+  const int MULAW_MAX = 0x1F;
+  int sign = encodedSample & 0x80;
+  int exponent = (encodedSample >> 4) & 0x07;
+  int mantissa = encodedSample & 0x0F;
+  int decodedSample = ((mantissa << 3) | 0x84) << (exponent - 1);
+  decodedSample += BIAS;
+  if (sign) {
+    decodedSample = -decodedSample;
+  }
+  return (int16_t)decodedSample;
+}
+};
+
+#endif // AUDIO_H
