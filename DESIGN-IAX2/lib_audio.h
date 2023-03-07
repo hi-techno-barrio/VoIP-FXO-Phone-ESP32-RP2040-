625009c77f5ebf43229acb5edfc918424e64a991
Christopher Coballes <cobecoballes@gmail.com>
2023-03-07 23:14:23 +0800

    Create lib_audio.h

---
+++ DESIGN-IAX2/lib_audio.h
@@ -0,0 +1,43 @@
+#ifndef AUDIO_H
+#define AUDIO_H
+
+#include <stdint.h>
+
+class AudioProcessing {
+  public:
+    static void filter(uint8_t *input, uint8_t *output, int inputSize, int outputSize);
+    static void amplify(uint8_t *input, uint8_t *output, int inputSize, int outputSize, float gain);
+    static void reduceNoise(uint8_t *input, uint8_t *output, int inputSize, int outputSize);
+};
+
+class Codec {
+  public:
+    virtual const char* getName() = 0;
+    virtual int getSampleRate() = 0;
+    virtual int getBitsPerSample() = 0;
+    virtual int getBlockSize() = 0;
+    virtual void compress(uint8_t *input, uint8_t *output, int inputSize) = 0;
+    virtual void decompress(uint8_t *input, uint8_t *output, int outputSize) = 0;
+};
+
+class uLawCodec : public Codec {
+  public:
+    const char* getName() { return "uLaw"; }
+    int getSampleRate() { return 8000; }
+    int getBitsPerSample() { return 8; }
+    int getBlockSize() { return 160; }
+    void compress(uint8_t *input, uint8_t *output, int inputSize);
+    void decompress(uint8_t *input, uint8_t *output, int outputSize);
+};
+
+class aLawCodec : public Codec {
+  public:
+    const char* getName() { return "aLaw"; }
+    int getSampleRate() { return 8000; }
+    int getBitsPerSample() { return 8; }
+    int getBlockSize() { return 160; }
+    void compress(uint8_t *input, uint8_t *output, int inputSize);
+    void decompress(uint8_t *input, uint8_t *output, int outputSize);
+};
+
+#endif
